70ee36ade82edd3bcc71ca391894d53581997f29
OneLiteFeather <schauderkristall@hotmail.de>
2023-07-11 16:39:41 +0200

    Add permission constant

---
+++ src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/Constants.java
@@ -0,0 +1,7 @@
+package net.onelitefeather.antiredstoneclockremastered.utils;
+
+public final class Constants {
+
+    public static final String PERMISSION_NOTIFY = "antiredstoneclockremasterd.notify.admin";
+
+}
