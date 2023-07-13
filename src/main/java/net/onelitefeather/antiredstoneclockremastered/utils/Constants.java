74ea5a33d2222b8d7ee81807b5e7f190844cc4f0
OneLiteFeather <schauderkristall@hotmail.de>
2023-07-13 14:03:00 +0200

    Add prefix

--- src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/Constants.java
+++ src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/Constants.java
@@ -4,4 +4,6 @@ public final class Constants {
 
     public static final String PERMISSION_NOTIFY = "antiredstoneclockremasterd.notify.admin";
 
+    public static final String PREFIX = "<gray></</gray><gradient:#c20300:#ff7f7a>Anti</gradient><gradient:#c20300:#ff7f7a>Redstone</gradient><gray>></gray>";
+
 }
