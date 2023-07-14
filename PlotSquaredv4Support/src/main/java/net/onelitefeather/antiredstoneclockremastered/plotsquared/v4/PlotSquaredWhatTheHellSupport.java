863f2bd06b9f5523d534a1a860f69c18e7805434
OneLiteFeather <schauderkristall@hotmail.de>
2023-07-14 11:21:30 +0200

    Add documentation

--- PlotSquaredv4Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v4/PlotSquaredWhatTheHellSupport.java
+++ PlotSquaredv4Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v4/PlotSquaredWhatTheHellSupport.java
@@ -8,6 +8,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class is for Plotsquared v4 support and is called "PlotSquaredWhatTheHellSupport" because it is legacy and
+ * Plotsquared v4 is no longer supported by the IntellectualSites team. Take sure to update Plotsquared, FAWE and
+ * your minecraft version to prevent not fixed issues and exploits.
+ * This class creates a new third party Plotsquared flag for enabling / disabling antiredstone detection on a plot.
+ * It also gets the accurate version of Plotsquared and checks if the location of the redstone clock is on a plot.
+ */
+
 public final class PlotSquaredWhatTheHellSupport extends AbstractPlotsquaredSupport {
 
     private static final PlotAPI PLOT_API = new PlotAPI();
@@ -17,6 +25,12 @@ public final class PlotSquaredWhatTheHellSupport extends AbstractPlotsquaredSupp
         PLOT_API.addFlag(new RedstoneClockFlag());
     }
 
+    /**
+     * Checks if the location of the redstone clock is in a plot
+     * @param location the location of the redstone clock
+     * @return true if the location is inside a plot and has the redstoneclockflag enabled,
+     * false if it is not inside a plot and has the redstoneclockflag disabled
+     */
     @Override
     public boolean isAllowedPlot(@NotNull Location location) {
         var plot = BukkitUtil.getPlot(location);
@@ -32,6 +46,11 @@ public final class PlotSquaredWhatTheHellSupport extends AbstractPlotsquaredSupp
 
         return false;
     }
+
+    /** This method is deprecated and checks the Plotsquared version to garantee matching support
+     *
+     * @return the plotsquared version of the Plotsquared plugin, if it is enabled, if not, return unknown
+     */
 
     @Override
     @SuppressWarnings("deprecation")
