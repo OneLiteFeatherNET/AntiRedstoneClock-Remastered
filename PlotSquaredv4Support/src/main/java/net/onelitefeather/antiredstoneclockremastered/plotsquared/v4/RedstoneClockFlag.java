4067d172f7c2f880a00a230f7f3ab295e5c045e4
OneLiteFeather <schauderkristall@hotmail.de>
2023-07-18 17:58:22 +0200

    Add documentation (creativity lost here)

--- PlotSquaredv4Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v4/RedstoneClockFlag.java
+++ PlotSquaredv4Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v4/RedstoneClockFlag.java
@@ -2,6 +2,10 @@ package net.onelitefeather.antiredstoneclockremastered.plotsquared.v4;
 
 import com.github.intellectualsites.plotsquared.plot.flag.BooleanFlag;
 public final class RedstoneClockFlag extends BooleanFlag {
+
+    /**
+     * Inherits the flag name
+     */
     public RedstoneClockFlag() {
         super(PlotSquaredWhatTheHellSupport.FLAG_STRING);
     }
