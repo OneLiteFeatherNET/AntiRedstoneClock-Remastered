bc8954156d8ae354827cda6bf4d1dbac624ae7c6
TheMeinerLP <p.glanz@madfix.me>
2024-01-29 17:59:49 +0100

    Cleanup constant

--- src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/Constants.java
+++ src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/Constants.java
@@ -10,8 +10,6 @@ public final class Constants {
 
     public static final String PERMISSION_NOTIFY = "antiredstoneclockremastered.notify.admin";
 
-    public static final String PREFIX = "<gray></</gray><gradient:#c20300:#ff7f7a>Anti</gradient><gradient:#c20300:#ff7f7a>Redstone</gradient><gray>></gray>";
-
     public static final Collection<Material> REDSTONE_ITEMS = new ArrayList<>(initRedstoneItems());
 
     private static Collection<Material> initRedstoneItems() {
