141ba499956cd9197d07986a6a25dbde32a00c1e
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-05-12 18:23:21 +0200

    Cleanup gradle and improve build workflow

--- PlotSquaredv6Support/build.gradle.kts
+++ PlotSquaredv6Support/build.gradle.kts
@@ -1,17 +1,5 @@
-plugins {
-    id("java")
-}
-
-repositories {
-    mavenCentral()
-    maven("https://maven.enginehub.org/repo/")
-    maven("https://papermc.io/repo/repository/maven-public/")
-}
-
 dependencies {
-    implementation(platform("com.intellectualsites.bom:bom-1.16.x:1.43"))
-    compileOnly("com.plotsquared:PlotSquared-Core")
-    compileOnly("com.plotsquared:PlotSquared-Bukkit") { isTransitive = false }
-    compileOnly(project(":internal-api"))
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    implementation(platform(libs.psv6.platform))
+    compileOnly(libs.psv6.core)
+    compileOnly(libs.psv6.bukkit) { isTransitive = false }
 }
