3ccb5653bb76b2e3272207010d2f6c0ec6c9e397
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-07-04 21:54:36 +0200

    Fix redstone clock flag

--- PlotSquaredv7Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v7/PlotSquaredModernSupport.java
+++ PlotSquaredv7Support/src/main/java/net/onelitefeather/antiredstoneclockremastered/plotsquared/v7/PlotSquaredModernSupport.java
@@ -2,6 +2,7 @@ package net.onelitefeather.antiredstoneclockremastered.plotsquared.v7;
 
 import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.PlotSquared;
+import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.flag.GlobalFlagContainer;
 import net.onelitefeather.antiredstoneclockremastered.api.AbstractPlotsquaredSupport;
 import org.bukkit.Bukkit;
@@ -18,13 +19,10 @@ public final class PlotSquaredModernSupport extends AbstractPlotsquaredSupport {
     @Override
     public boolean isAllowedPlot(@NotNull Location location) {
         var plotArea = PlotSquared.get().getPlotAreaManager().getPlotArea(BukkitUtil.adapt(location));
-        if (plotArea != null) {
-            var flag = plotArea.getFlagContainer().getFlag(RedstoneClockFlag.class);
-            if (flag != null) {
-                return flag.getValue();
-            }
-        }
-        return true;
+        if (plotArea == null) return false;
+        Plot plot = plotArea.getPlot(BukkitUtil.adapt(location));
+        if (plot == null) return false;
+        return plot.getFlag(RedstoneClockFlag.class);
     }
 
     @Override
