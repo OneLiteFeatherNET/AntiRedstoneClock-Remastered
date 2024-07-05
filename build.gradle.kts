c5f193b9423b7523c1ff018866e876dad92c5cbf
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-07-05 16:35:02 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ if (!File("$rootDir/.git").exists()) {
 }
 
 group = "net.onelitefeather"
-version = "1.1.0"
+version = "1.2.0"
 val supportedMinecraftVersions = listOf(
     "1.19.4",
     "1.20.6",
