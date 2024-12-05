6cec569b82805b02bb023eb412e42d7a8e4cb14a
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-12-05 10:48:41 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ if (!File("$rootDir/.git").exists()) {
 }
 
 group = "net.onelitefeather"
-version = "1.2.1"
+version = "1.2.2"
 val supportedMinecraftVersions = listOf(
     "1.19.4",
     "1.20.6",
