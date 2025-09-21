8f841c07c9b6797a62eba8fffc4978199cf7f223
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-09-21 12:19:42 +0200

    feat: add FoliaHelper and RegionServiceFactory for platform-specific service creation

--- src/main/java/net/onelitefeather/antiredstoneclockremastered/service/factory/RegionServiceFactory.java
+++ src/main/java/net/onelitefeather/antiredstoneclockremastered/service/factory/RegionServiceFactory.java
@@ -1,9 +1,12 @@
 package net.onelitefeather.antiredstoneclockremastered.service.factory;
 
 import net.onelitefeather.antiredstoneclockremastered.AntiRedstoneClockRemastered;
+import net.onelitefeather.antiredstoneclockremastered.injection.PlatformModule;
 import net.onelitefeather.antiredstoneclockremastered.service.api.RedstoneClockService;
 import net.onelitefeather.antiredstoneclockremastered.service.api.RegionService;
 import net.onelitefeather.antiredstoneclockremastered.service.impl.BukkitRedstoneClockService;
+import net.onelitefeather.antiredstoneclockremastered.service.impl.BukkitRegionService;
+import net.onelitefeather.antiredstoneclockremastered.service.impl.FoliaRegionService;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,20 +14,20 @@ import org.slf4j.LoggerFactory;
 import static net.onelitefeather.antiredstoneclockremastered.service.factory.FoliaHelper.isFolia;
 
 /**
- * Factory for creating RedstoneClockService implementations.
- * This factory determines the appropriate implementation based on the server platform.
+ * Factory for creating instances of RegionService based on the server environment.
  *
+ * @since 2.3.0-beta.10
+ * @version 1.0.0
  * @author TheMeinerLP
- * @version 2.2.0
- * @since 1.0.0
  */
-public final class RedstoneClockServiceFactory {
+public class RegionServiceFactory {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RedstoneClockServiceFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegionServiceFactory.class);
 
-    private RedstoneClockServiceFactory() {
+    private RegionServiceFactory() {
         // Utility class
     }
+
 
     /**
      * Creates the appropriate RedstoneClockService implementation for the current platform.
@@ -33,15 +36,13 @@ public final class RedstoneClockServiceFactory {
      * @return the appropriate RedstoneClockService implementation
      */
     @NotNull
-    public static RedstoneClockService createService(@NotNull AntiRedstoneClockRemastered plugin, RegionService regionService) {
+    public static RegionService createService(@NotNull AntiRedstoneClockRemastered plugin) {
         if (isFolia()) {
-            LOGGER.info("Folia detected - using FoliaRedstoneClockService");
-            // Uncomment when ready to enable Folia support:
-            // return new FoliaRedstoneClockService(plugin);
-            LOGGER.warn("Folia implementation not yet ready, falling back to Bukkit implementation");
+            LOGGER.info("Folia detected - using FoliaRegionService");
+            return new FoliaRegionService(plugin);
         }
-        
-        LOGGER.info("Using BukkitRedstoneClockService");
-        return new BukkitRedstoneClockService(plugin, regionService);
+
+        LOGGER.info("Using BukkitRegionService");
+        return new BukkitRegionService(plugin);
     }
 }
