4e0a346d4ce34368f1138ba410b17f9a44816431
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-11-03 11:54:18 +0100

    feat: add UUIDTagType for handling UUIDs in PersistentDataType

---
+++ src/main/java/net/onelitefeather/antiredstoneclockremastered/utils/UUIDTagType.java
@@ -0,0 +1,39 @@
+package net.onelitefeather.antiredstoneclockremastered.utils;
+
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public final class UUIDTagType implements PersistentDataType<byte[], UUID> {
+
+    public static final UUIDTagType UUID = new UUIDTagType();
+
+    @Override
+    public Class<byte[]> getPrimitiveType() {
+        return byte[].class;
+    }
+
+    @Override
+    public Class<UUID> getComplexType() {
+        return UUID.class;
+    }
+
+    @Override
+    public byte[] toPrimitive(UUID complex, PersistentDataAdapterContext context) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(complex.getMostSignificantBits());
+        bb.putLong(complex.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    @Override
+    public UUID fromPrimitive(byte[] primitive, PersistentDataAdapterContext context) {
+        ByteBuffer bb = ByteBuffer.wrap(primitive);
+        long firstLong = bb.getLong();
+        long secondLong = bb.getLong();
+        return new UUID(firstLong, secondLong);
+    }
+
+}
